d3d640f48fe39cd8201b72e5b40c5a515e463c47
jonathangl2 <jonathangl2@outlook.com>
2025-12-11 00:37:05 +0000

    Deploying to gh-pages from @ jonathangl2/latinadvisor-nextjs@77f6b584c579966f0506ba0a811a104ce688f771 🚀

--- _next/static/vkSCMKliigRBEEp4S3Nxg/_buildManifest.js
+++ _next/static/vkSCMKliigRBEEp4S3Nxg/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/latinadvisor-nextjs//_next/:path+",
-        "destination": "/latinadvisor-nextjs/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
